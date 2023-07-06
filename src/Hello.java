00905000f228bb4b0f97bab25ea639d3a0befd9c
moonsbay <moonsbay@example.com>
2023-07-06 12:10:16 +0900

    color fg bg

--- src/Hello.java
+++ src/Hello.java
@@ -2,7 +2,9 @@
 public class Hello {
 	public static void main(String[] args){
 		System.out.println("\033[2j");
+		System.out.println("\033[2j");
 		System.out.println("Hello Java!");
+		System.out.println("\033[0m");
 	}
 
 	
