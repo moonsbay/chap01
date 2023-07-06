66079a34e5e503d2d7269655b802a9087d6df346
moonsbay <moonsbay@gmail.com>
2023-07-06 12:01:30 +0900

    vt100 clear

--- src/Hello.java
+++ src/Hello.java
@@ -1,6 +1,9 @@
 
-class Hello {
+public class Hello {
 	public static void main(String[] args){
-		System.out.println("Hello");
+		System.out.println("\033[2j");
+		System.out.println("Hello Java!");
 	}
+
+	
 }
--- src/Triangle.java
+++ src/Triangle.java
@@ -2,7 +2,9 @@
 class Triangle {
 
 	public static void main(String[] args) {
-		System.out.println("#####");
+		// 삼각형모양 깨짐
+		// 어리나ㅓ이라얼나는이라ㅓㅣㄴ
+                System.out.println("#####");
 		System.out.println( "####");
 		System.out.println(  "###");
 		System.out.println(   "##");
