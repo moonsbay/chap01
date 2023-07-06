e1a1ddebd038f87fd3c0e070967c8d32865635d6
moonsbay <moonsbay@example.com>
2023-07-06 16:42:24 +0900

    eclipse 적용

--- src/Triangle.java
+++ src/Triangle.java
@@ -4,7 +4,7 @@ class Triangle {
 	public static void main(String[] args) {
 		// 삼각형모양 깨짐
 		// 어리나ㅓ이라얼나는이라ㅓㅣㄴ
-                System.out.println("#####");
+		System.out.println("#####");
 		System.out.println( "####");
 		System.out.println(  "###");
 		System.out.println(   "##");
